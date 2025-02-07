d75b219d1b175957c3ffee97df92fd82e6c0322d
syedafizza410 <162727504+syedafizza410@users.noreply.github.com>
2025-02-07 23:58:46 +0500

    add

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['cdn.sanity.io', 'res.cloudinary.com'], 
     },
+    env: {
+        NEXTAUTH_URL: 'https://figma-hackathon-inky.vercel.app/', 
+    },
 };
 
 export default nextConfig;
